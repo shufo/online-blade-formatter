8a8146bf109a5edfc0bb223ae7c62e5c8c5b56a4
shufo <meikyowise@gmail.com>
2020-12-09 00:30:09 +0900

    style: 💄 use laravel logo as favicon
    svg is from https://commons.wikimedia.org/wiki/File:Laravel.svg. thanks

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
