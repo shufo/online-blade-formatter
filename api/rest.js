70b828868715b12ed73d4e6500593ef618d75c68
shufo <meikyowise@gmail.com>
2020-12-12 21:51:25 +0900

    feat: 🎸 add swr header

--- api/rest.js
+++ api/rest.js
@@ -8,6 +8,7 @@ app.all('/format', async (req, res) => {
   try {
     const formatter = new Formatter()
     const formatted = await formatter.formatContent(req.body.data)
+    res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
     res.json({ data: formatted })
   } catch (error) {
     res.status(400).send(error.toString())
