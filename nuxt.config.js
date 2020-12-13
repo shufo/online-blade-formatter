7becc8aab5bb22ea4aba2764ed3900b9deccaf85
shufo <meikyowise@gmail.com>
2020-12-13 21:00:05 +0900

    fix: 🐛 change build target to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,10 @@
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   // ssr: false,
-  ssr: true,
+  ssr: false,
 
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'server',
+  target: 'static',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
